4bf33aed808039b93c7e61af64dbf6752ab7ddc3
haena3230 <gosk0912@naver.com>
2020-06-04 19:24:50 +0900

    first

--- src/Screens/LoginScreen/index.tsx
+++ src/Screens/LoginScreen/index.tsx
@@ -1,46 +1,125 @@
-// Login
+// Login.tsx
 import React from 'react';
-import {StyleSheet, Text, TextInput, View, Button} from 'react-native';
+import Reinput from 'reinput';
+//reinput 모듈 다운
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from 'react-native';
 
 function LoginScreen({navigation}) {
+  //state = {email: '', password: '', errorMessage: null};
+  // handleLogin = () => {
+  //   // TODO: Firebase stuff...
+  //   console.log('handleLogin');
+  // };
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-      <TextInput
-        style={styles.textInput}
-        autoCapitalize="none"
-        placeholder="Email"
-      />
-      <TextInput
-        secureTextEntry
-        style={styles.textInput}
-        autoCapitalize="none"
-        placeholder="Password"
-      />
-      <Button
-        title="로그인"
-        onPress={() => navigation.navigate('MainScreen')}
-      />
-      <Button
-        title="계정 없으면 가입하세요"
-        onPress={() => navigation.navigate('SignUpScreen')}
-      />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.imageview}>
+          <Image source={require('~/Assets/login_nyang@mdpi.png')} />
+        </View>
+        <View style={styles.formcontainer}>
+          <Reinput
+            style={styles.inputemail}
+            underlineColor="#b1b2b2"
+            labelActiveColor="#999999"
+            underlineActiveColor="#999999"
+            label="Email"
+            // onChangeText={email => this.setState({email})}
+            // value={this.state.email}
+          />
+          <Reinput
+            style={styles.inputemail}
+            secureTextEntry
+            underlineColor="#b1b2b2"
+            labelActiveColor="#999999"
+            underlineActiveColor="#999999"
+            label="Password"
+            //onChangeText={password => this.setState({password})}
+            // value={this.state.password}
+          />
+          <TouchableOpacity
+            style={styles.button}
+            activeOpacity={0.8}
+            onPress={() => navigation.navigate('MainScreen')}>
+            <Text style={styles.buttontext}>로그인</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.buttonjoin}
+            activeOpacity={0.8}
+            onPress={() => navigation.navigate('SignUpScreen')}>
+            <Text style={styles.buttontextjoin}>회원가입</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    //전체 container
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#FCE0E0',
     alignItems: 'center',
+    justifyContent: 'center',
   },
-  textInput: {
-    height: 40,
+  imageview: {
+    //이미지
+    alignItems: 'center',
+    marginTop: 35,
+    zIndex: 1,
+    top: 23,
+  },
+  formcontainer: {
+    //요소 있는 container
+    flex: 1,
+    backgroundColor: 'white',
+    zIndex: 0,
     width: '90%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginTop: 8,
+    padding: 20,
+    alignItems: 'center',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  inputemail: {
+    //이메일, 비밀번호 적는 칸
+    width: '85%',
+    height: 50,
+    marginVertical: 5,
+  },
+  button: {
+    //로그인 버튼
+    alignItems: 'center',
+    backgroundColor: '#b1b2b2',
+    padding: 10,
+    width: '90%',
+    borderRadius: 20,
+    margin: 24,
+  },
+  buttontext: {
+    //로그인 버튼 text
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  buttonjoin: {
+    //회원가입 버튼
+    alignItems: 'center',
+    padding: 10,
+    width: '90%',
+    borderRadius: 20,
+  },
+  buttontextjoin: {
+    //회원가입 버튼 text
+    color: 'black',
+    fontSize: 13,
+    marginTop: 50, //임시
   },
 });
 
--- src/Screens/SignUpScreen/index.tsx
+++ src/Screens/SignUpScreen/index.tsx
@@ -1,31 +1,49 @@
-// SignUp.tsx
+// SignUp Screen
 import React from 'react';
-import {StyleSheet, Text, TextInput, View, Button} from 'react-native';
+import Reinput from 'reinput';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 function SignUpScreen({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>Sign Up</Text>
-      <TextInput
-        placeholder="Email"
-        autoCapitalize="none"
+      <Reinput
         style={styles.textInput}
-        // onChangeText={email => this.setState({email})}
-        // value={this.state.email}
-      />
-      <TextInput
         secureTextEntry
-        placeholder="Password"
-        autoCapitalize="none"
+        underlineColor="#b1b2b2"
+        labelActiveColor="#999999"
+        underlineActiveColor="#999999"
+        label="Email"
+      />
+      <Reinput
         style={styles.textInput}
-        // onChangeText={password => this.setState({password})}
-        // value={this.state.password}
+        secureTextEntry
+        label="Password"
+        underlineColor="#b1b2b2"
+        labelActiveColor="#999999"
+        underlineActiveColor="#999999"
       />
-      <Button title="가입하기" onPress={() => navigation.navigate('MainScreen')} />
-      <Button
-        title="계정이 있다면 로그인 ㄱ"
-        onPress={() => navigation.navigate('LoginScreen')}
+      <Reinput
+        style={styles.textInput}
+        secureTextEntry
+        label="Password Check"
+        underlineColor="#b1b2b2"
+        labelActiveColor="#999999"
+        underlineActiveColor="#999999"
       />
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={() => navigation.navigate('MainScreen')}>
+        <Text style={styles.buttontext}>가입하기</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={() => navigation.navigate('LoginScreen')}>
+        <Text style={styles.buttontext}>
+          이미 계정을 가지셨나요? 로그인해주세요
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -33,16 +51,28 @@ function SignUpScreen({navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: 'white',
     alignItems: 'center',
   },
   textInput: {
-    height: 40,
+    height: 50,
     width: '90%',
-    borderColor: 'gray',
-    borderWidth: 1,
     marginTop: 8,
   },
+  button: {
+    //버튼
+    alignItems: 'center',
+    backgroundColor: '#b1b2b2',
+    padding: 10,
+    width: '90%',
+    borderRadius: 20,
+    margin: 24,
+  },
+  buttontext: {
+    //버튼 text
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
 });
-
 export default SignUpScreen;
